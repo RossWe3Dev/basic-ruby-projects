2febf5254ec851be45b0b5db55e9c6b1967582ea
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-25 16:48:04 +0200

    Edit script name typo, clean the code
    Changed script name to correct a typo.
    Removed debugger, removed final p, updated CLI prompts to add user's
    interactivity.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,15 +1,13 @@
-def ceasar_cipher(string,cipher_key)
+def caesar_cipher(string,cipher_key)
 
   number_arr = string.each_char.map do |char|
   char = (/[A-Za-z]/).match?(char)? char.ord : char   # #ord converts only a-z characters into integers
   end
 
-  debugger
-
   mod_char_array = number_arr.map do |n_ord|    # accepted integers from #ord method are 
     if (n_ord.to_i.between?(65, 90)) then             # (A = 65, Z = 90)
       new_ord = ((n_ord - 65 + cipher_key) % 26) + 65
-      new_char = new_ord.chr
+      new_char = new_ord.chr    
     elsif (n_ord.to_i.between?(97, 122)) then         # (a = 97, z = 122)
       new_ord = ((n_ord - 97 + cipher_key) % 26) + 97
       new_char = new_ord.chr
@@ -19,9 +17,15 @@ def ceasar_cipher(string,cipher_key)
   end
 
   mod_string = mod_char_array.join
-  p mod_string
+  mod_string
 end
 
-ceasar_cipher('ab cz! Z',8)    # test
 
-#ceasar_cipher(gets.chomp,gets.chomp.to_i)
+puts "What string would you like to encrypt?"
+string = gets.chomp
+
+puts "What will be the key to decipher your secret message?"
+cipher_key = gets.chomp.to_i
+
+encrypted_string = caesar_cipher(string,cipher_key)
+puts "Encrypted message: '#{encrypted_string}'"
