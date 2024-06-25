08f5fc8412fc0cc568d137382f1573871d94e6b5
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-25 10:27:54 +0200

    Refactor code, add VSCode debugger

--- ceasar-cipher.rb
+++ ceasar-cipher.rb
@@ -1,26 +1,21 @@
-def ceasar_cipher(string,key)
+def ceasar_cipher(string,cipher_key)
   arr = string.split('')
+
+debugger
 
   number_arr = []
   arr.each do |char|
-    if (/[A-Za-z]/).match?(char)
-      number_arr << char.ord    #ord converts only a-z characters into integers
-    else
-      number_arr << char
-    end
+    converted_character = (/[A-Za-z]/).match?(char)? char.ord : char   #ord converts only a-z characters into integers
+    number_arr << converted_character
   end
 
-  mod_num_arr = number_arr.map do |n|
-    if (n.is_a? Integer)
-      n+key
-    else
-      n
-    end
+mod_num_arr = number_arr.map do |n|
+    n = (n.is_a? Integer)? n+cipher_key : n
   end
 
   mod_char_arr = mod_num_arr.map do |new_n| 
     if new_n.is_a? Integer
-      new_n.chr     #chr converts the integers back to characters
+      new_n.chr     #chr converts the integers back to characters (a = 97, z = 122)(A = 65, Z = 90)
     else
       new_n
     end
@@ -30,4 +25,6 @@ def ceasar_cipher(string,key)
   p mod_string
 end
 
-ceasar_cipher(gets.chomp,gets.chomp.to_i)
+ceasar_cipher('ab c',3)
+
+#ceasar_cipher(gets.chomp,gets.chomp.to_i)
