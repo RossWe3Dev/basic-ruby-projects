993575b23a4b6a088748f3710f4bafdee28e17fe
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-25 19:19:16 +0200

    Clean the code, delete useless comments

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,33 +1,18 @@
 def caesar_cipher(string,cipher_key)
 
-  # number_arr = string.each_char.map do |char|
-  #   char = (/[A-Za-z]/).match?(char)? char.ord : char    # #ord converts only A-Z or a-z characters into integers
-  # end
+    # #ord converts characters into integers, #chr integers into characters
 
   mod_char_array = string.each_char.map do |char|
-    if (/[A-Z]/).match?(char) then
-      mod_char = ((char.ord - 65 + cipher_key) % 26) + 65
+    if (/[A-Z]/).match?(char)                               # used letter to integer ranges from #ord method are
+      mod_char = ((char.ord - 65 + cipher_key) % 26) + 65   # (A = 65, Z = 90)
       mod_char = mod_char.chr
-    elsif (/[a-z]/).match?(char) then
-      mod_char = ((char.ord - 97 + cipher_key) % 26) + 97
+    elsif (/[a-z]/).match?(char)
+      mod_char = ((char.ord - 97 + cipher_key) % 26) + 97   # (a = 97, z = 122)
       mod_char = mod_char.chr
     else
       char
     end
   end
-
-
-  # modified_char_array = number_arr.map do |char_ord|     # accepted integers from #ord method are 
-  #   if (char_ord.between?(65, 90)) then                  # (A = 65, Z = 90)
-  #     new_ord = ((char_ord - 65 + cipher_key) % 26) + 65
-  #     new_char = new_ord.chr
-  #   elsif (char_ord.between?(97, 122)) then              # (a = 97, z = 122)
-  #     new_ord = ((char_ord - 97 + cipher_key) % 26) + 97
-  #     new_char = new_ord.chr
-  #   else
-  #     char_ord
-  #   end
-  # end
 
   mod_string = mod_char_array.join
   mod_string
