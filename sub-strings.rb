b549dc72ad0d2c8cc1f6d9534d33edd63bd25a75
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-27 18:22:17 +0200

    Refactor the code to include sub-strings count
    Removed select and added iteration with #each that pushes matching word
    into the matching_words_arr.
    To achieve sub-string matching a second iteration using #each goes
    through the given collection and checks is str_word includes dict_word
    and it's not identical, if so it pushes the match into the array.
    Edited variables names for consistency.

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,19 +1,23 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string,collection)
-  arr_str = string.downcase.split(/[\W]/)
-  p arr_str
+  words_arr = string.downcase.split(/[\W]/)
+  p words_arr
 
-  matching_arr_str = arr_str.select do |word|
-    collection.include?(word)
+  matching_words_arr = []
+  words_arr.each do |str_word|
+    matching_words_arr << str_word if collection.include?(str_word)
+    collection.each do |dict_word|
+      matching_words_arr << dict_word if (str_word.include?(dict_word) && str_word != dict_word)
+    end
   end
-  p matching_arr_str
+  p matching_words_arr
 
-  match = matching_arr_str.reduce(Hash.new(0)) do |match_key, word|
+  match = matching_words_arr.reduce(Hash.new(0)) do |match_key, word|
     match_key[word] += 1
     match_key
   end
   p match
 end
 
-substrings("below, low hills below", dictionary)
+substrings("Below, low hills!", dictionary)
