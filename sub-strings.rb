0a81844bee8d5bab89edfe95c81aab6f18838ee9
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-27 18:38:28 +0200

    Clean the code, improve CLI output

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,23 +1,26 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i", "e","low","own","part","partner","sit"]
 
-def substrings(string,collection)
+def substrings(string,dictionary)
   words_arr = string.downcase.split(/[\W]/)
-  p words_arr
 
   matching_words_arr = []
   words_arr.each do |str_word|
-    matching_words_arr << str_word if collection.include?(str_word)
-    collection.each do |dict_word|
+    matching_words_arr << str_word if dictionary.include?(str_word)
+    dictionary.each do |dict_word|
       matching_words_arr << dict_word if (str_word.include?(dict_word) && str_word != dict_word)
     end
   end
-  p matching_words_arr
 
   match = matching_words_arr.reduce(Hash.new(0)) do |match_key, word|
     match_key[word] += 1
     match_key
   end
+
+  puts "This where the matches in our dictionary:"
   p match
 end
 
-substrings("Below, low hills!", dictionary)
+puts "What string do you want to check?"
+string = gets.chomp
+
+substrings(string, dictionary)
