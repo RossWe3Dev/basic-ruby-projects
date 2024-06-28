21efb60c92fda4bca77c37c1d8b983d4dcd13017
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-28 15:57:54 +0200

    Complete bubble-sort assignment

---
+++ bubble-sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(arr)
+  n = arr.length
+  swapped = true
+
+  while swapped == true do
+    for i in 0...n-1
+      swapped = false
+      if arr[i] > arr[i+1]
+      arr[i], arr[i+1] = arr[i+1], arr[i]
+      swapped = true
+      end
+    end
+    n = n-1
+  end
+  p arr
+end
+
+bubble_sort([4,3,78,2,0,2])
+bubble_sort([6,2,67,1,34,8,13])
