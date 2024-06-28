88e66c00ce63d060cb0f537a08e7e5e382a56422
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-28 10:03:39 +0200

    Refactor and clean the code
    Streamlined logic behind substring matching. Deleted redundant line, now
    it the method directly pushes dictionary words that are included or
    completely match each str_word.

--- sub-strings.rb
+++ sub-strings.rb
@@ -4,20 +4,19 @@ def substrings(string,dictionary)
   words_arr = string.downcase.split(/[\W]/)
 
   matching_words_arr = []
-  words_arr.each do |str_word|
-    matching_words_arr << str_word if dictionary.include?(str_word)
-    dictionary.each do |dict_word|
-      matching_words_arr << dict_word if (str_word.include?(dict_word) && str_word != dict_word)
+  dictionary.each do |dict_word|
+    words_arr.each do |str_word|
+      matching_words_arr << dict_word if str_word.include?(dict_word)
     end
   end
 
-  match = matching_words_arr.reduce(Hash.new(0)) do |match_key, word|
+  matches_hash = matching_words_arr.reduce(Hash.new(0)) do |match_key, word|
     match_key[word] += 1
     match_key
   end
 
   puts "This where the matches in our dictionary:"
-  p match
+  p matches_hash
 end
 
 puts "What string do you want to check?"
