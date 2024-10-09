832577d5c94619a09d4e12c1181bde7f0ef3da6b
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-10-09 11:46:05 +0200

    Optimize sorting algorithm
    Modified test arrays, corrected incorrect logic and optimized algorithm
    based on wikipedia documentation.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,20 +2,21 @@
 
 def bubble_sort(arr)
   n = arr.length
-  swapped = true
 
-  while swapped
-    (0...n - 1).each do |i|
-      swapped = false
-      if arr[i] > arr[i + 1]
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        swapped = true
+  while n > 1
+    new_n = 0
+    (1..n - 1).each do |i|
+      if arr[i - 1] > arr[i]
+        arr[i - 1], arr[i] = arr[i], arr[i - 1]
+        new_n = i
       end
+
+      puts "#{arr} at iteration #{i}"
     end
-    n -= 1
+    n = new_n
   end
   p arr
 end
 
 bubble_sort([4, 3, 78, 2, 0, 2])
-bubble_sort([6, 2, 67, 1, 34, 8, 13])
+bubble_sort([6, 2, 67, 1, 34, 1, 8, 13])
